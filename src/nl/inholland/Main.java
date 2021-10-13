b39ecc2cf9820836ce769d3ba5c5584ab8c898fc
Daniela Repkova <@student.inholland.nl>
2021-10-13 19:25:58 +0200

    assignment 1 of week 1

---
+++ src/nl/inholland/Main.java
@@ -0,0 +1,17 @@
+package nl.inholland;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        System.out.println(name + " " + age);
+
+    }
+}
