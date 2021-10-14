b60851b66a7d3babbc25bf147d05ae0dc00401e8
Daniela Repkova <@student.inholland.nl>
2021-10-14 17:14:12 +0200

    interface in method

--- src/nl/inholland/Employee.java
+++ src/nl/inholland/Employee.java
@@ -11,4 +11,5 @@ public class Employee implements Payable {
     public double getPayout() {
         return grossSalary - grossSalary * tax;
     }
+
 }
--- src/nl/inholland/Main.java
+++ src/nl/inholland/Main.java
@@ -4,11 +4,15 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
+    static void displayPayment(Payable payable, String person) {
+        System.out.println("Salary of " + person + " is: " + payable.getPayout());
+    }
     public static void main(String[] args) {
-    Employee employee = new Employee(1300);
-    System.out.println("Salary of this employee is: " + employee.getPayout());
-    Freelancer freelancer = new Freelancer(15, 60);
-    System.out.println("Salary of this freelancer is: " + freelancer.getPayout());
+    Employee employee = new Employee(3000);
+    displayPayment(employee, "employee");
+    Freelancer freelancer = new Freelancer(60, 140);
+    displayPayment(freelancer, "freelancer");
     }
 
 }
