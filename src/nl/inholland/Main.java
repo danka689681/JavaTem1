fc458ba282865da8477a8b6714dc18b46ff29d1e
Daniela Repkova <@student.inholland.nl>
2021-10-14 18:48:14 +0200

    sorted collection

--- src/nl/inholland/Main.java
+++ src/nl/inholland/Main.java
@@ -1,29 +1,22 @@
 package nl.inholland;
-import java.text.DecimalFormat;
 import java.util.*;
 
 public class Main {
 
+    public static void createStudent(Map students, String lastName, String firstName) {
+        Student student = new Student(lastName, firstName);
+        students.put(student.getFirstName(), student.getLastName());
+    }
+
     public static void main(String[] args) {
-        List<String> words = new ArrayList<>();
-        words.add("Blue");
-        words.add("Black");
-        words.add("Yellow");
-        words.add("Cat");
-        words.add("OK");
-        words.add("UnicornIsVeryEpic");
-        words.add("Popcorn");
-        words.add("SomeOtherWordWithALoootOfChars");
-        words.add("I");
-        words.add("GuessThatIsIt");
+        Map<String, String> students = new TreeMap<>();
+        createStudent(students, "Laz", "Tim");
+        createStudent(students, "Surname", "Al");
+        createStudent(students, "Lopez", "Luke");
+        createStudent(students, "Moore", "Diana");
+        createStudent(students, "Schack", "Vina");
+        createStudent(students, "Schack", "Vina");
 
-        Map<Integer, String> map = new TreeMap<>();
-        for (String color : words) {
-            map.put(color.length(), color);
-        }
-
-        for (Map.Entry<Integer, String> entry : map.entrySet()) {
-            System.out.println(String.format("%d : %s", entry.getKey(), entry.getValue()));
-        }
+        System.out.println(students);
     }
 }
---
+++ src/nl/inholland/Student.java
@@ -0,0 +1,20 @@
+package nl.inholland;
+
+public class Student {
+    String firstName;
+    String lastName;
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+}
