a3dc3079a44969ff826a41c873e88f768b51066a
Daniela Repkova <@student.inholland.nl>
2021-10-14 18:19:08 +0200

    map

--- src/nl/inholland/Main.java
+++ src/nl/inholland/Main.java
@@ -1,13 +1,29 @@
 package nl.inholland;
 import java.text.DecimalFormat;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        MiniTheater miniTheater = new MiniTheater();
-        miniTheater.initSeats();
-    }
+        List<String> words = new ArrayList<>();
+        words.add("Blue");
+        words.add("Black");
+        words.add("Yellow");
+        words.add("Cat");
+        words.add("OK");
+        words.add("UnicornIsVeryEpic");
+        words.add("Popcorn");
+        words.add("SomeOtherWordWithALoootOfChars");
+        words.add("I");
+        words.add("GuessThatIsIt");
 
+        Map<Integer, String> map = new TreeMap<>();
+        for (String color : words) {
+            map.put(color.length(), color);
+        }
+
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println(String.format("%d : %s", entry.getKey(), entry.getValue()));
+        }
+    }
 }
